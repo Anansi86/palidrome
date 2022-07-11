572c701e2f5b048caf52d9e63bca190f17733361
Shawn Staton <shawnstaton@Shawns-MacBook-Pro.local>
2022-07-11 14:58:19 -0400

    reverse string

--- app.js
+++ app.js
@@ -12,3 +12,13 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2. */
 
+// First make function take the string and reverse it
+
+function palindrome(str) {
+    let backwardString = str.split("").reverse().join("");
+   // return true;
+   console.log(backwardString); 
+}
+  
+  palindrome("Shawn");
+ 
