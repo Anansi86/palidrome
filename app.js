3aa4d9f9b58d41cfc0e224301aa3df1fa79e538d
Shawn Staton <shawnstaton@Shawns-MacBook-Pro.local>
2022-07-12 13:53:03 -0400

    no_spaces_or_symbols

--- app.js
+++ app.js
@@ -13,14 +13,23 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2. */
 
 // First make function take the string and reverse it
-// Now to make my function make all non-alpanumeric figures lower case
+// Now to make my function make all alpanumeric figures lower case
+// Next I need to remove spaces and non-aplanumeric characters
 
 function palindrome(str) {
-    let backwardString = str.split("").reverse().join("").toLowerCase();
+    let regEx = /[!"#$%&'()*+,-.0-9/:;<=>?@[\]^_`{|}~]/g;
+    let backwardString = str
+    .split(" ")
+    .reverse()
+    .join("")
+    .toLowerCase()
+    .replace(regEx, '')
+    ;
+
    // return true;
    console.log(backwardString); 
 }
   
-  palindrome("Shawn");
+  palindrome("Shawn!#4 Eye");
  
 //testing out this branch
